2787c6e9a32a02525de2bf43eed98358885364ec
Joseph Durel <jadematrix.art@gmail.com>
2014-05-19 21:43:38 -0400

    Added new program icon, updated launch arg documentation in launch.sh

--- src/launch.sh
+++ src/launch.sh
@@ -4,4 +4,7 @@ cd ..
 ./MacOS/BQTDraw -l../../BQTDraw.txt -d -t2
 
 # Options:
-# 
+# [ -d | --devmode     ]            Enables developer mode options
+# [ -l | --logfile     ] FILE       Sets a log file, none by default
+# [ -t | --taskthreads ] UINT       Limits the max number of task threads; 0 = no limit
+# Options are applied in order, so it is recommended to change the log file first to log any init errors
