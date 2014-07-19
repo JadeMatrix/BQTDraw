81013cf113621f37d7cc553189b5dbfe9ba5f149
JadeMatrix <jadematrix.art@gmail.com>
2014-07-18 22:28:31 -0400

    Changed launch_osx.sh to log to a crash file on unexpected exit; made a few other minor changes

--- resources/launch_osx.sh
+++ resources/launch_osx.sh
@@ -1,13 +1,28 @@
 #!/bin/sh
+
 cd "${0%/*}"
 cd ..
-./MacOS/BQTDraw -l../../BQTDraw.txt -d -p~/.BQTDraw.cfg
+
+# Linux release
+# ./BQTDraw -l~/.BQTDraw_log.txt -d -p~/.BQTDraw.cfg > ~/BQTDraw_crashlog.txt
+
+# OS X release
+# ./MacOS/BQTDraw -l~/.BQTDraw_log.txt -p~/Application\ Support/BQTDraw/BQTDraw.cfg > ~/BQTDraw_crashlog.txt
+
+# OS X devel
+./MacOS/BQTDraw -l../../BQTDraw_log.txt -d -p~/Application\ Support/BQTDraw/BQTDraw.cfg > -l../../BQTDraw_crashlog.txt
+
+if [ test -s ~/BQTDraw_crashlog.txt ] ; then
+    echo BQTDraw has crashed, see BQTDraw_crashlog.txt
+    # echo BQTDraw has crashed, see ~/BQTDraw_crashlog.txt
+else
+    rm -f ~/BQTDraw_crashlog.txt
 
 # Options:
-# [ -h | --help         ]            Prints usage guide & exits
-# [ -v | --version      ]            Prints the software version & exits
-# [ -f | --open-file    ] FILE       Opens file on startup
-# [ -l | --log-file     ] FILE       Sets a log file, none by default
-# [ -d | --dev-mode     ]            Enables developer mode options
-# [ -t | --task-threads ] UINT       Limits the max number of task threads; 0 = no limit
+# [ -h | --help ] Prints usage guide & exits
+# [ -v | --version ] Prints the software version & exits
+# [ -f | --open-file ] FILE Opens file on startup
+# [ -l | --log-file ] FILE Sets a log file, none by default
+# [ -d | --dev-mode ] Enables developer mode options
+# [ -t | --task-threads ] UINT Limits the max number of task threads; 0 = no limit
 # Options are applied in order, so it is recommended to change the log file first to log any init errors
